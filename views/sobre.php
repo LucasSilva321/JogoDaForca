e36ea3849aee6ee93fd92ae201007a93f7b070d1
Lucas F. da Silva <luckkass89@gmail.com>
2018-02-23 18:01:28 -0300

    Update sobre.php

--- views/sobre.php
+++ views/sobre.php
@@ -2,7 +2,7 @@
 <h2>Lucas Feitoza da Silva</h2>
 <div style="height: 25px;margin-top: 5px">
 	<img src="/assets/images/git.png" height="25px" > 
-	<a href="https://github.com/lucasftt" style="line-height: 25px;">https://github.com/lucasftt</a>
+	<a href="https://github.com/lucasfts" style="line-height: 25px;">https://github.com/lucasfts</a>
 </div>
 <br><br>
 
